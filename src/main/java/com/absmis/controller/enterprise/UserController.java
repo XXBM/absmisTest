124626bee5bb33c8d2996ce4ffa0e54fe1a8ae75
XXBM <1961346638@qq.com>
2017-08-10 10:36:19 -0500

    重置密码

---
+++ src/main/java/com/absmis/controller/enterprise/UserController.java
@@ -0,0 +1,23 @@
+package com.absmis.controller.enterprise;
+
+import com.absmis.domain.authority.User;
+import com.absmis.service.authority.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+    //重置密码
+    @RequestMapping(value = "/resetPsd", method = RequestMethod.POST)
+    public User resetPsd(@RequestParam Long id)throws Exception {
+        User user = userService.findById(id);
+        user.setPassword(user.getUsername());
+        userService.update(user);
+        return user;
+    }
+}
