8448a2430b2ce6efaad6c56b2ba584972b6ce25f
XXBM <1961346638@qq.com>
2017-09-07 15:24:27 -0500

    后台分页

--- src/main/java/com/absmis/controller/enterprise/ApplicationStructureTypeController.java
+++ src/main/java/com/absmis/controller/enterprise/ApplicationStructureTypeController.java
@@ -1,10 +1,12 @@
 package com.absmis.controller.enterprise;
 
 import com.absmis.domain.enterprise.ApplicationStructureType;
+import com.absmis.domain.message.DataGridResult;
 import com.absmis.service.enterprise.ApplicationStructureTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +16,15 @@ public class ApplicationStructureTypeController {
     @Autowired
     ApplicationStructureTypeService applicationStructureTypeService;
 
-
+    //实现分页
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
+    public DataGridResult findAll(
+            @RequestParam(value = "page") Integer page,
+            @RequestParam(value = "rows") Integer size
+    )throws Exception {
+        DataGridResult countrylists = this.applicationStructureTypeService.getCountryList(page,size);
+        return countrylists;
+    }
 
     /**
      * 获取到所有
