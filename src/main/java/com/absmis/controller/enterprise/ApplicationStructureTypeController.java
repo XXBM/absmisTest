287fd34972e73685e2e6283f997ce50517bc8d45
XXBM <1961346638@qq.com>
2017-09-13 12:13:21 -0500

    分页

--- src/main/java/com/absmis/controller/enterprise/ApplicationStructureTypeController.java
+++ src/main/java/com/absmis/controller/enterprise/ApplicationStructureTypeController.java
@@ -9,20 +9,65 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ApplicationStructureTypeController {
     @Autowired
     ApplicationStructureTypeService applicationStructureTypeService;
+/**
+ * @Author: LihuaHuang
+ * @Description:
+ *  1.每次翻页都修改SQL，向SQL传入相关参数去数据库实时查出该页的数据并显示。
+ *  2.查出数据库某张表的全部数据，再通过在业务逻辑里面进行处理去取得某些数据并显示。
+ *  其中第一种是最常用的，但有时候你必须在业务逻辑里面对数据进行处理，也就是第二种方法，
+ *  在此，介绍第二种方法
+ * @Date: Created in ${TIME} ${DATE}.
+ * @Modified by:
+ */
+    //实现分页
+    @RequestMapping(value = "/testPage", method = RequestMethod.GET)
+    public Map<String, Object> testPage(
+            @RequestParam(value = "page") Integer page,
+            @RequestParam(value = "rows") Integer size
+    ) throws Exception {
+        //每页显示的条数
+        int pageSize = size;
+        List<ApplicationStructureType> applicationStructureTypes = this.applicationStructureTypeService.findAllT();
+        Map<String, Object> map = new HashMap<String, Object>();
+        //查到的总用户数
+        map.put("total", applicationStructureTypes.size());
+
+        //总页数
+        int pageTimes;
+        if (applicationStructureTypes.size() % pageSize == 0) {
+            pageTimes = applicationStructureTypes.size() / pageSize;
+        } else {
+            pageTimes = applicationStructureTypes.size() / pageSize + 1;
+        }
+        map.put("pageTimes", pageTimes);
+
+        List<ApplicationStructureType> newApplicationStructureTypes = new ArrayList<ApplicationStructureType>();
+        //每页开始的第几条记录
+        if(pageTimes==page) {
+            newApplicationStructureTypes.addAll(applicationStructureTypes.subList((page-1)*size,applicationStructureTypes.size()));
+        }else {
+            newApplicationStructureTypes.addAll(applicationStructureTypes.subList((page-1)*size,(page-1)*size+size));
+        }
+        map.put("applicationStructureType", newApplicationStructureTypes);
+        return map;
+    }
 
     //实现分页
     @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     public DataGridResult findAll(
             @RequestParam(value = "page") Integer page,
             @RequestParam(value = "rows") Integer size
-    )throws Exception {
-        DataGridResult countrylists = this.applicationStructureTypeService.getCountryList(page,size);
+    ) throws Exception {
+        DataGridResult countrylists = this.applicationStructureTypeService.getCountryList(page, size);
         return countrylists;
     }
 
@@ -30,7 +75,7 @@ public class ApplicationStructureTypeController {
      * 获取到所有
      */
     @RequestMapping(value = "/findAllApplicationStructureTypes", method = RequestMethod.GET)
-    public List<ApplicationStructureType> findApplicationStructureType()throws Exception {
+    public List<ApplicationStructureType> findApplicationStructureType() throws Exception {
         List<ApplicationStructureType> applicationStructureTypes = applicationStructureTypeService.findAllT();
         return applicationStructureTypes;
     }
--- src/main/java/com/absmis/service/enterprise/ApplicationStructureTypeService.java
+++ src/main/java/com/absmis/service/enterprise/ApplicationStructureTypeService.java
@@ -9,6 +9,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -27,37 +28,37 @@ public class ApplicationStructureTypeService extends BasicService<ApplicationStr
     }
 
     public DataGridResult getCountryList(int page, int rows) {
-//        //分页处理
-//        PageHelper.startPage(page, rows);
-//
-//        //查询结果
-//        Page<ApplicationStructureType> list = applicationStructureTypeRepository.findAll(new PageRequest(page - 1, rows));
-//        List<ApplicationStructureType> list2 = applicationStructureTypeRepository.findAll();
-//        //获取分页信息
-//        PageInfo<ApplicationStructureType> info = new PageInfo<>(list.getContent());
-//        DataGridResult result = new DataGridResult();
-//        long total = info.getTotal();
-//
-//        //封装分页信息
-//        List<ApplicationStructureType> row = info.getList();
-//        result.setRows(row);
-//        result.setTotal((long)list2.size());
-//        return result;
         //分页处理
         PageHelper.startPage(page, rows);
 
         //查询结果
-        List<ApplicationStructureType> list = applicationStructureTypeRepository.findAll();
-
+        Page<ApplicationStructureType> list = applicationStructureTypeRepository.findAll(new PageRequest(page - 1, rows));
+        List<ApplicationStructureType> list2 = applicationStructureTypeRepository.findAll();
         //获取分页信息
-        PageInfo<ApplicationStructureType> info = new PageInfo<ApplicationStructureType>(list);
+        PageInfo<ApplicationStructureType> info = new PageInfo<>(list.getContent());
         DataGridResult result = new DataGridResult();
         long total = info.getTotal();
 
         //封装分页信息
         List<ApplicationStructureType> row = info.getList();
         result.setRows(row);
-        result.setTotal(total);
+        result.setTotal((long)list2.size());
         return result;
+        //分页处理
+//        PageHelper.startPage(page, rows);
+//
+//        //查询结果
+//        List<ApplicationStructureType> list = applicationStructureTypeRepository.findAll();
+//
+//        //获取分页信息
+//        PageInfo<ApplicationStructureType> info = new PageInfo<ApplicationStructureType>(list);
+//        DataGridResult result = new DataGridResult();
+//        long total = info.getTotal();
+//
+//        //封装分页信息
+//        List<ApplicationStructureType> row = info.getList();
+//        result.setRows(row);
+//        result.setTotal(total);
+//        return result;
     }
 }
